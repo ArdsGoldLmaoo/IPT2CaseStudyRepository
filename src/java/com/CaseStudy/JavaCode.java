2c81b1ade4d9f95ef8eaa28e60f15f633759cd26
ArdsGoldLmaoo <72840819+ArdsGoldLmaoo@users.noreply.github.com>
2021-03-18 17:19:59 +0800

    Added Netbeans project folder
    - Added project folder/s generated by Netbeans with one function for testing.

---
+++ src/java/com/CaseStudy/JavaCode.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.CaseStudy;
+import javax.jws.*;
+/**
+ *
+ * @author TSM3A Group
+ */
+@WebService
+public class JavaCode {
+    @WebMethod
+    public double number(double a,double b){
+    return b;}
+    
+}
